c736fda5e9f883e12a9dd6472780fe5c3b00cea3
Arno Richter <arno.richter+github@gmail.com>
2022-11-21 10:35:44 +0100

    Update build.py

--- build.py
+++ build.py
@@ -1,8 +1,11 @@
 import string
 import random
+from datetime import datetime
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
+dt = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+
 with open("README.md", "a") as myfile:
-    myfile.write("\n appended python %s" % id_generator())
+    myfile.write("  \n- appended python %s (random string %s)" % (dt, id_generator()))
