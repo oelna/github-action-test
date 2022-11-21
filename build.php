0254708759ab7f6a9c6151d4000ef4693799e791
Arno Richter <arno.richter+github@gmail.com>
2022-11-21 10:45:31 +0100

    Update build.php

--- build.php
+++ build.php
@@ -1,5 +1,5 @@
 <?php
 
-	$txt = " appended php " . gmdate("Y-m-d H:i:s") . " (random string " . substr(md5(rand()), 0, 7) . ")";
+	$txt = "- appended php " . gmdate("Y-m-d H:i:s") . " (random string " . substr(md5(rand()), 0, 7) . ")";
 
-	$myfile = file_put_contents('README.md', PHP_EOL.'- '.$txt , FILE_APPEND | LOCK_EX);
+	$myfile = file_put_contents('README.md', PHP_EOL.$txt , FILE_APPEND | LOCK_EX);
